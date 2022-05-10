8a6dffc3f1f88f3bae231b1c1808e72504eb9f6e
Carlos Thadeu <cthadeusantos@gmail.com>
2022-05-09 22:46:38 -0300

    fix index.html wrong directory to main.js add toFixed at area_necessaria_ambiente

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ function main(){
     document.getElementById("info_janelas").innerHTML = info_janelas();
     document.getElementById("info_renovacao").innerHTML = info_renovacao();
     document.getElementById("observacao2").innerHTML = observacao2();
-    document.getElementById("area_necessaria").innerHTML = area_necessaria_ambiente();
+    document.getElementById("area_necessaria").innerHTML = area_necessaria_ambiente().toFixed(2);
 };
 
